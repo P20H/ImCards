b1c3717c7c0d88d6b988c9f3b0a8c10916d0da83
Cambloid <Dr.Niete@gmail.com>
2021-02-04 15:00:38 +0100

    Move imgui to imframework
    Update setup

--- Setup.bat
+++ Setup.bat
@@ -3,17 +3,29 @@
 mkdir ThirdParty
 cd ThirdParty
 
-git clone --branch docking https://github.com/ocornut/imgui.git imgui
-git clone --branch 2020.11-1 https://github.com/microsoft/vcpkg.git vcpkg
-git clone https://github.com/CoreTrackProject/ImFramework.git
-git clone https://github.com/juliettef/imgui_markdown.git
+if not exist vcpkg (
+     git clone --branch 2020.11-1 https://github.com/microsoft/vcpkg.git vcpkg
+     cd vcpkg
 
-cd vcpkg
+     cmd /c call ./bootstrap-vcpkg.bat
+     cmd /c vcpkg.exe install glfw3 glew --triplet x64-windows
 
-cmd /c call ./bootstrap-vcpkg.bat
-cmd /c vcpkg.exe install glfw3 glew --triplet x64-windows
+     cd..
+)
 
-cd../..
+if not exist ImFramework (
+     git clone https://github.com/CoreTrackProject/ImFramework.git
+)
+
+if not exist imgui_markdown (
+     git clone https://github.com/juliettef/imgui_markdown.git
+)
+
+cd ..
+
+
+
+
 
 mkdir Build
 cd Build
